c2a950f23a8b582bdda57b6df14db7fcdc83a7f0
sanjayaditya90 <sanjayaditya90@gmail.com>
2026-07-15 12:02:16 +0530

    Initial commit

---
+++ src/main/java/com/samsup/user/User.java
@@ -0,0 +1,128 @@
+package com.samsup.user;
+
+import java.time.LocalDate;
+
+public class User {
+
+	private String userId;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNo;
+	private String address;
+	private String age;
+	private LocalDate createDate;
+	private String createdBy;
+	private String modifiedBy;
+	private LocalDate modifiedDate;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public LocalDate getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDate modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public void login(String userId, String password) {
+
+	}
+}
---
+++ src/main/java/com/samsup/user/UserLogin.java
@@ -0,0 +1,7 @@
+package com.samsup.user;
+
+public class UserLogin {
+	public void login(String userId, String password) {
+		
+	}
+}
